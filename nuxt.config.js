841930a71f47d12b61a0484ac54e79a221408550
muhammad-gf <muhammadmaseyev@gmail.com>
2021-05-01 11:16:58 +0500

    Fifth commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,89 +1,90 @@
 export default {
-        // Global page headers: https://go.nuxtjs.dev/config-head
-        head: {
-                title: "tujjor",
-                htmlAttrs: {
-                        lang: "en"
-                },
-                meta: [
-                        { charset: "utf-8" },
-                        {
-                                name: "viewport",
-                                content: "width=device-width, initial-scale=1"
-                        },
-                        { hid: "description", name: "description", content: "" }
-                ],
-                link: [
-                        {
-                                rel: "icon",
-                                type: "image/x-icon",
-                                href: "/favicon.ico"
-                        },
-                        {
-                                rel: "stylesheet",
-                                type: "text/css",
-                                href:
-                                        "//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"
-                        },
-                        {
-                                rel: "stylesheet",
-                                type: "text/css",
-                                href:
-                                        "//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"
-                        },
-
-                        {
-                                rel: "stylesheet",
-                                href:
-                                        "https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
-                        },
-
-                        {
-                                rel: "stylesheet",
-                                href:
-                                        "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-                        },
-
-                        {
-                                rel: "stylesheet",
-                                href:
-                                        "https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap"
-                        }
-                ]
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: "tujjor",
+        htmlAttrs: {
+            lang: "en"
         },
-
-        // Global CSS: https://go.nuxtjs.dev/config-css
-        css: ["@/assets/scss/main.scss"],
-
-        // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-        plugins: [
-                { src: "@/plugins/vue-slick-carousel.js" },
-                { src: "~/plugins/vue-slider-component", ssr: false }
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1"
+            },
+            { hid: "description", name: "description", content: "" }
         ],
+        link: [
+            {
+                rel: "icon",
+                type: "image/x-icon",
+                href: "/favicon.ico"
+            },
+            {
+                rel: "stylesheet",
+                type: "text/css",
+                href:
+                    "//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"
+            },
+            {
+                rel: "stylesheet",
+                type: "text/css",
+                href:
+                    "//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"
+            },
 
-        // Auto import components: https://go.nuxtjs.dev/config-components
-        components: true,
+            {
+                rel: "stylesheet",
+                href:
+                    "https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
+            },
 
-        // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-        buildModules: [],
+            {
+                rel: "stylesheet",
+                href:
+                    "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            },
 
-        // Modules: https://go.nuxtjs.dev/config-modules
-        modules: [
-                // https://go.nuxtjs.dev/bootstrap
-                "bootstrap-vue/nuxt",
-                // https://go.nuxtjs.dev/axios
-                "@nuxtjs/style-resources",
-                "@nuxtjs/axios"
-        ],
-        styleResources: {
-                scss: ["@/assets/scss/*.scss"]
-        },
-        // Axios module configuration: https://go.nuxtjs.dev/config-axios
-        axios: {},
+            {
+                rel: "stylesheet",
+                href:
+                    "https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap"
+            }
+        ]
+    },
 
-        // Build Configuration: https://go.nuxtjs.dev/config-build
-        build: {
-                transpile: ["vue-slick"],
-                vendor: ["vue-slider-component"]
-        }
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: ["@/assets/scss/main.scss"],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+        { src: "@/plugins/vue-slick-carousel.js" },
+        { src: "@/plugins/vue-slider-component.js", ssr: false },
+        { src: "@/plugins/vue-star-rating.js", mode: "client", ssr: false }
+    ],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/bootstrap
+        "bootstrap-vue/nuxt",
+        // https://go.nuxtjs.dev/axios
+        "@nuxtjs/style-resources",
+        "@nuxtjs/axios"
+    ],
+    styleResources: {
+        scss: ["@/assets/scss/*.scss"]
+    },
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {},
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        transpile: ["vue-slick"],
+        vendor: ["vue-slider-component", "star-rating"]
+    }
 };
--- pages/product_show.vue
+++ pages/product_show.vue
@@ -272,12 +272,233 @@
                         </div>
                     </div>
                 </div>
+
+                <div class="col-lg-8 clients__review__container">
+                    <h4 class="clients__review__header ">
+                        Отзывы покупателей
+                        <span>
+                            (24)
+                        </span>
+                    </h4>
+
+                    <div class="client__review__box row">
+                        <div
+                            class="client__review__heading  col-lg-6 col-sm-8 col-12  d-flex justify-content-between align-items-start"
+                        >
+                            <h4 class="client__review__name">
+                                Турсунов Асрор
+                            </h4>
+
+                            <star-rating
+                                class="star__rating"
+                                :increment="0.5"
+                                inactive-color="#219EBC"
+                                :star-size="18"
+                                :show-rating="false"
+                                :rating="4.5"
+                            ></star-rating>
+                        </div>
+                        <div class="client__review__txt col-sm-12">
+                            <span>
+                                Качество ткани хорошее, но если вы предпочитаете
+                                оверсайз, выбирайте размер меньше вашего,
+                                например, слишком свободные рукава.
+                            </span>
+                        </div>
+                        <div
+                            class="client__review__date col-lg-6 col-sm-8 col-12 d-flex justify-content-between align-items-center"
+                        >
+                            <span class="client__review--date">
+                                30.04.2021
+                            </span>
+
+                            <div class="client__buy--svg">
+                                <svg
+                                    width="16"
+                                    height="16"
+                                    viewBox="0 0 16 16"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M16 8C16 10.1217 15.1571 12.1566 13.6569 13.6569C12.1566 15.1571 10.1217 16 8 16C5.87827 16 3.84344 15.1571 2.34315 13.6569C0.842855 12.1566 0 10.1217 0 8C0 5.87827 0.842855 3.84344 2.34315 2.34315C3.84344 0.842855 5.87827 0 8 0C10.1217 0 12.1566 0.842855 13.6569 2.34315C15.1571 3.84344 16 5.87827 16 8ZM12.03 4.97C11.9586 4.89882 11.8735 4.84277 11.7799 4.80522C11.6863 4.76766 11.5861 4.74936 11.4853 4.75141C11.3845 4.75347 11.2851 4.77583 11.1932 4.81717C11.1012 4.85851 11.0185 4.91797 10.95 4.992L7.477 9.417L5.384 7.323C5.24183 7.19052 5.05378 7.1184 4.85948 7.12183C4.66518 7.12525 4.47979 7.20397 4.34238 7.34138C4.20497 7.47879 4.12625 7.66418 4.12283 7.85848C4.1194 8.05278 4.19152 8.24083 4.324 8.383L6.97 11.03C7.04128 11.1012 7.12616 11.1572 7.21958 11.1949C7.313 11.2325 7.41305 11.2509 7.51375 11.2491C7.61444 11.2472 7.71374 11.2251 7.8057 11.184C7.89766 11.1429 7.9804 11.0837 8.049 11.01L12.041 6.02C12.1771 5.8785 12.2523 5.68928 12.2504 5.49296C12.2485 5.29664 12.1698 5.10888 12.031 4.97H12.03Z"
+                                        fill="#219EBC"
+                                    />
+                                </svg>
+                                <span>
+                                    Я купил товар
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="client__review__box row">
+                        <div
+                            class="client__review__heading  col-lg-6 col-sm-8 col-12 d-flex justify-content-between align-items-start"
+                        >
+                            <h4 class="client__review__name">
+                                Ахмад Собиров
+                            </h4>
+
+                            <star-rating
+                                class="star__rating"
+                                :increment="0.5"
+                                inactive-color="#219EBC"
+                                :star-size="18"
+                                :show-rating="false"
+                                :rating="5"
+                            ></star-rating>
+                        </div>
+                        <div class="client__review__txt col-sm-12">
+                            <span>
+                                Ткань по своей фактуре - это не ткань, которая
+                                создает очень мелкие морщинки, занимает все ваше
+                                тело, качество проявляется в любом виде.
+                            </span>
+                        </div>
+                        <div
+                            class="client__review__date col-lg-6 col-sm-8 col-12 d-flex justify-content-between align-items-center"
+                        >
+                            <span class="client__review--date">
+                                21.04.2021
+                            </span>
+
+                            <div class="client__buy--svg">
+                                <svg
+                                    width="16"
+                                    height="16"
+                                    viewBox="0 0 16 16"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M16 8C16 10.1217 15.1571 12.1566 13.6569 13.6569C12.1566 15.1571 10.1217 16 8 16C5.87827 16 3.84344 15.1571 2.34315 13.6569C0.842855 12.1566 0 10.1217 0 8C0 5.87827 0.842855 3.84344 2.34315 2.34315C3.84344 0.842855 5.87827 0 8 0C10.1217 0 12.1566 0.842855 13.6569 2.34315C15.1571 3.84344 16 5.87827 16 8ZM12.03 4.97C11.9586 4.89882 11.8735 4.84277 11.7799 4.80522C11.6863 4.76766 11.5861 4.74936 11.4853 4.75141C11.3845 4.75347 11.2851 4.77583 11.1932 4.81717C11.1012 4.85851 11.0185 4.91797 10.95 4.992L7.477 9.417L5.384 7.323C5.24183 7.19052 5.05378 7.1184 4.85948 7.12183C4.66518 7.12525 4.47979 7.20397 4.34238 7.34138C4.20497 7.47879 4.12625 7.66418 4.12283 7.85848C4.1194 8.05278 4.19152 8.24083 4.324 8.383L6.97 11.03C7.04128 11.1012 7.12616 11.1572 7.21958 11.1949C7.313 11.2325 7.41305 11.2509 7.51375 11.2491C7.61444 11.2472 7.71374 11.2251 7.8057 11.184C7.89766 11.1429 7.9804 11.0837 8.049 11.01L12.041 6.02C12.1771 5.8785 12.2523 5.68928 12.2504 5.49296C12.2485 5.29664 12.1698 5.10888 12.031 4.97H12.03Z"
+                                        fill="#219EBC"
+                                    />
+                                </svg>
+                                <span>
+                                    Я купил товар
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="client__review__box row">
+                        <div
+                            class="client__review__heading  col-lg-6 col-sm-8 col-12 d-flex justify-content-between align-items-start"
+                        >
+                            <h4 class="client__review__name">
+                                Улугбек Мустаков
+                            </h4>
+
+                            <star-rating
+                                class="star__rating"
+                                :increment="0.5"
+                                inactive-color="#219EBC"
+                                :star-size="18"
+                                :show-rating="false"
+                                :rating="4"
+                            ></star-rating>
+                        </div>
+                        <div class="client__review__txt col-sm-12">
+                            <span>
+                                Полноразмерное качество красивое.
+                            </span>
+                        </div>
+                        <div
+                            class="client__review__date  col-lg-6 col-sm-8 col-12 d-flex justify-content-between align-items-center"
+                        >
+                            <span class="client__review--date">
+                                26.04.2021
+                            </span>
+
+                            <div class="client__buy--svg">
+                                <svg
+                                    width="16"
+                                    height="16"
+                                    viewBox="0 0 16 16"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M16 8C16 10.1217 15.1571 12.1566 13.6569 13.6569C12.1566 15.1571 10.1217 16 8 16C5.87827 16 3.84344 15.1571 2.34315 13.6569C0.842855 12.1566 0 10.1217 0 8C0 5.87827 0.842855 3.84344 2.34315 2.34315C3.84344 0.842855 5.87827 0 8 0C10.1217 0 12.1566 0.842855 13.6569 2.34315C15.1571 3.84344 16 5.87827 16 8ZM12.03 4.97C11.9586 4.89882 11.8735 4.84277 11.7799 4.80522C11.6863 4.76766 11.5861 4.74936 11.4853 4.75141C11.3845 4.75347 11.2851 4.77583 11.1932 4.81717C11.1012 4.85851 11.0185 4.91797 10.95 4.992L7.477 9.417L5.384 7.323C5.24183 7.19052 5.05378 7.1184 4.85948 7.12183C4.66518 7.12525 4.47979 7.20397 4.34238 7.34138C4.20497 7.47879 4.12625 7.66418 4.12283 7.85848C4.1194 8.05278 4.19152 8.24083 4.324 8.383L6.97 11.03C7.04128 11.1012 7.12616 11.1572 7.21958 11.1949C7.313 11.2325 7.41305 11.2509 7.51375 11.2491C7.61444 11.2472 7.71374 11.2251 7.8057 11.184C7.89766 11.1429 7.9804 11.0837 8.049 11.01L12.041 6.02C12.1771 5.8785 12.2523 5.68928 12.2504 5.49296C12.2485 5.29664 12.1698 5.10888 12.031 4.97H12.03Z"
+                                        fill="#219EBC"
+                                    />
+                                </svg>
+                                <span>
+                                    Я купил товар
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <a href="#" class="clients__review__btn">
+                        Показать больше комментариев
+                    </a>
+                </div>
+
+                <div class="col-lg-4 customer__review__container">
+                    <div class="customer__review__box row">
+                        <h4 class="customer__review__header col-sm-12">
+                            Оставьте ваш отзыв
+                        </h4>
+
+                        <input
+                            type="text"
+                            placeholder="Ваше имя"
+                            class="customer__review__input col-sm-12"
+                        />
+
+                        <input
+                            type="text"
+                            placeholder="Ваш e-mail"
+                            class="customer__review__input col-sm-12"
+                        />
+
+                        <textarea
+                            name="review"
+                            class="customer__review__input customer__review__txt col-sm-12"
+                            placeholder="Ваш отзыв"
+                        >
+                        </textarea>
+
+                        <a href="#" class="customer__review__send--btn ">
+                            Отправить отзыв
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
-<script></script>
+<script>
+export default {
+    data() {
+        return {
+            rating: "No Rating Selected",
+            currentRating: "No Rating",
+            currentSelectedRating: "No Current Rating",
+            boundRating: 3
+        };
+    },
+    methods: {
+        setRating: function(rating) {
+            this.rating = "You have Selected: " + rating + " stars";
+        },
+        showCurrentRating: function(rating) {
+            this.currentRating =
+                rating === 0
+                    ? this.currentSelectedRating
+                    : "Click to select " + rating + " stars";
+        },
+        setCurrentSelectedRating: function(rating) {
+            this.currentSelectedRating =
+                "You have Selected: " + rating + " stars";
+        }
+    }
+};
+</script>
 
 <style lang="scss" scoped>
 .slick-slider {
@@ -611,6 +832,163 @@
     }
 }
 
+.clients__review__container {
+    .clients__review__header {
+        margin-bottom: 30px;
+        font-family: Roboto, sans-serif;
+        font-weight: 500;
+        font-size: 24px;
+        line-height: 100%;
+        /* identical to box height, or 24px */
+        color: #000000;
+        span {
+            color: #219ebc;
+        }
+    }
+
+    .client__review__box {
+        margin-bottom: 20px;
+
+        .client__review__heading {
+            .client__review__name {
+                margin-bottom: 10px;
+                font-family: Roboto, sans-serif;
+                font-weight: 500;
+                font-size: 20px;
+                line-height: 130%;
+                /* identical to box height, or 26px */
+                color: #000000;
+            }
+
+            .star__rating {
+                width: 127px;
+            }
+        }
+
+        .client__review__txt {
+            margin-bottom: 20px;
+            span {
+                font-family: Roboto;
+                font-size: 16px;
+                line-height: 130%;
+                /* or 21px */
+                color: #666666;
+            }
+        }
+
+        .client__review__date {
+            .client__review--date {
+                font-family: Roboto;
+                font-size: 14px;
+                line-height: 130%;
+                /* or 18px */
+                color: #666666;
+            }
+
+            .client__buy--svg {
+                span {
+                    font-family: Roboto;
+                    font-size: 16px;
+                    line-height: 130%;
+                    /* identical to box height, or 21px */
+                    color: #666666;
+                }
+            }
+        }
+    }
+
+    .clients__review__btn {
+        display: inline-block;
+        margin: 20px 0;
+        padding: 12px 24px;
+        border: 1px solid #666666;
+        border-radius: 5px;
+
+        // TEXT STYLING
+        font-family: Roboto, sans-serif;
+        font-size: 14px;
+        line-height: 100%;
+        /* identical to box height, or 14px */
+        color: #666666;
+    }
+}
+
+.customer__review__container {
+    background-color: blueviolet;
+
+    .customer__review__box {
+        margin: 0;
+        padding: 30px;
+        max-width: 373px;
+        background-color: #fce3a4;
+        border-radius: 5px;
+
+        .customer__review__header {
+            font-family: Roboto, sans-serif;
+            font-size: 24px;
+            line-height: 1;
+            /* or 31px */
+            color: #023047;
+            margin-bottom: 20px;
+            padding: 0;
+        }
+
+        .customer__review__input {
+            display: inline-block;
+            padding: 15px 12px;
+            font-family: Roboto, sans-serif;
+            font-size: 14px;
+            line-height: 100%;
+            /* identical to box height, or 14px */
+            &::-webkit-input-placeholder {
+                color: #c4c4c4;
+                font-style: italic;
+            }
+            border: none;
+            border-radius: 3px;
+
+            color: #000;
+            height: 1em;
+            margin-bottom: 20px;
+        }
+
+        .customer__review__txt {
+            height: 7em;
+            line-height: 1.3;
+            padding: 10px 12px;
+        }
+
+        .customer__review__send--btn {
+            margin: auto;
+            padding: 12px 33px;
+            background-color: #fe9e0d;
+            border-radius: 5px;
+
+            color: #ffffff;
+            font-family: Roboto, sans-serif;
+            font-weight: 500;
+            font-size: 16px;
+            line-height: 1;
+            /* identical to box height, or 16px */
+
+            transition: all 0.2s;
+            @include button__bg--color;
+            @include hover__active--animation;
+        }
+    }
+}
+
+@media only screen and (max-width: 992px) {
+    //  TO SET ENTER REVIEW SEND BOX
+    .customer__review__container {
+        padding: 80px 15px;
+        .customer__review__box {
+            margin-left: auto;
+            margin-right: auto;
+        }
+    }
+}
+
 @media only screen and (max-width: 952px) {
     .product-info {
         h4 {
@@ -677,8 +1055,6 @@
 
         .product__info--txt {
             font-size: 14px;
-            span {
-            }
         }
 
         .product__info--description {
@@ -706,11 +1082,50 @@
     }
 }
 
+@media only screen and (max-width: 576px) {
+    // STYLING REVIEW CONTAINER FOR MOBILE DEVICE
+    .clients__review__container {
+        .client__review__box {
+            margin-bottom: 60px;
+
+            .client__review__heading {
+                .star__rating {
+                    width: auto;
+                }
+            }
+
+            .client__review__txt {
+                margin-bottom: 10px;
+            }
+        }
+
+        .clients__review__btn {
+            display: block;
+            padding: 16px 24px;
+            // TEXT STYLING
+            text-align: center;
+        }
+    }
+
+    .customer__review__container {
+        padding: 60px 0;
+    }
+}
+
 @media only screen and (max-width: 500px) {
     .product-info {
         .select__size {
             display: flex;
             flex-direction: column;
+        }
+    }
+}
+
+@media only screen and (max-width: 377px) {
+    // DELETING BORDER ON REVIEW SEND BOX
+    .customer__review__container {
+        .customer__review__box {
+            border-radius: 0;
         }
     }
 }
